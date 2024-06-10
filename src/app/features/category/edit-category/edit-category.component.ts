bf4420390dbc2c58ec9111358edf71f4d82e422f
mdqahtani <MDQAHTANI@gadd.gov.sa>
2024-06-10 14:45:22 +0300

    UI update feature

--- src/app/features/category/edit-category/edit-category.component.ts
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../models/category.model';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-edit-category',
   templateUrl: './edit-category.component.html',
   styleUrls: ['./edit-category.component.css']
 })
-export class EditCategoryComponent implements OnInit, OnDestroy{
+export class EditCategoryComponent implements OnInit,OnDestroy{
 
   id: string | null = null;
   paramsSubscription?: Subscription;
+  editCategorySubscription?: Subscription;
   category?: Category;
 
   constructor (private route: ActivatedRoute,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private router: Router
   ){}
   
 
@@ -28,7 +32,7 @@ export class EditCategoryComponent implements OnInit, OnDestroy{
         if(this.id){
           this.categoryService.getCategoryById(this.id)
           .subscribe({
-            next: (response) =>{
+            next: (response) => {
               this.category = response;
             }
           })
@@ -38,12 +42,27 @@ export class EditCategoryComponent implements OnInit, OnDestroy{
     })
   }
 
-  onFormSubmit():void{
-    console.log(this.category);
+  onFormSubmit(): void{
+    const updateCategoryRequest: UpdateCategoryRequest = {
+      name: this.category?.name ?? '',
+      urlhandle: this.category?.urlHandle ?? ''
+    };
+
+    //pass this object tp service
+    if (this.id){
+      this.editCategorySubscription =  this.categoryService
+      .updateCategory(this.id, updateCategoryRequest)
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/Categories');
+        }
+      });
+    }
   }
+  
   ngOnDestroy(): void {
 
     this.paramsSubscription?.unsubscribe();
-
+    this.editCategorySubscription?.unsubscribe();
   }
 }
